da6ba46f89d8cc6df89df6c49d95b4d3e0f3f009
Alexey Viklov <alexey.viklov@gmail.com>
2024-12-01 14:36:00 +0300

    Update: styles and animations

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,16 +1,19 @@
 import './Popup.scss'
+import { motion, AnimatePresence } from 'motion/react'
 
 export default function Popup({ children, isOpen, onClose }) {
     return (
-        <div className={isOpen ? "popup-container" : "popup-container hidden"}>
-            <div className="popup">
-                <div className="popup-content-container">
-                    {children}
-                </div>
-                <div className="popup-buttons-container">
-                    <button onClick={() => onClose()}>Close</button>
-                </div>
-            </div>
-        </div>
+        <AnimatePresence>
+            {isOpen && <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className={"popup-container"}>
+                <motion.div initial={{ scale: 0, translateY: "50vh" }} animate={{ scale: 1, translateY: "0" }} exit={{ scale: .5, translateY: "50vh" }} className="popup">
+                    <div className="popup-content-container">
+                        {children}
+                    </div>
+                    <div className="popup-buttons-container">
+                        <button onClick={() => onClose()}>close</button>
+                    </div>
+                </motion.div>
+            </motion.div>}
+        </AnimatePresence>
     )
 }
